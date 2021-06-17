48cccd98ac35ac98823eab2138d4a936cea72172
Kumail Habib Mirza <kumailhabib12@gmail.com>
2021-06-18 00:55:32 +0500

    updated: input for strings using whitespaces

--- user-input/main.go
+++ user-input/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+)
 
 func main() {
 	// declaring vars for name
@@ -17,4 +22,14 @@ func main() {
 	// concating both names for creating a full name
 	fullname := fname + " " + lname;
 	fmt.Printf("Your Full Name is %v.\n", fullname)
+
+	// fmt.scanln doesnt accept string with whitespaces
+	fmt.Print("Enter a string with whitespaces: ");
+	// fmt.scanln doesnt accept string with whitespaces
+	scanner := bufio.NewReader(os.Stdin)
+    s, _:= scanner.ReadString('\n');
+
+	s = strings.TrimSuffix(s, "\n")
+	fmt.Printf("String with Whitespaces: %v\n", s);
+
 }
